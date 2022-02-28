c614582d5302636e337ddb86c80e516735529542
vector2myth <vector2myth@icloud.com>
2022-02-28 21:09:21 +0800

    2.6 制作CopyUtil封装BeanUtils

--- src/main/java/com/wug/wiki/service/EbookService.java
+++ src/main/java/com/wug/wiki/service/EbookService.java
@@ -5,6 +5,7 @@ import com.wug.wiki.domain.EbookExample;
 import com.wug.wiki.mapper.EbookMapper;
 import com.wug.wiki.req.EbookReq;
 import com.wug.wiki.resp.EbookResp;
+import com.wug.wiki.utils.CopyUtil;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -24,16 +25,23 @@ public class EbookService {
         criteria.andNameLike("%"+req.getName()+"%"); // 模糊匹配
         List<Ebook> ebookList=ebookMapper.selectByExample(ebookExample);
 
-        // 从ebook拷贝到ebookresp里去
-        List<EbookResp> respList=new ArrayList<>();     // 新建一个list对象
-        for (Ebook ebook : ebookList) {
-            // 对Ebook列表进行迭代，然后拷贝到EbookResp这个List里去
-            EbookResp ebookResp=new EbookResp();    // 1、新建一个接收对象
-            BeanUtils.copyProperties(ebook,ebookResp);  // 2、使用Spring提供的BeanUitls.copyProperties()，参数为传递对象和接收对象
-            respList.add(ebookResp);    // 3、将对象传递给respList
-        }
+//        // 从ebook拷贝到ebookresp里去
+//        List<EbookResp> respList=new ArrayList<>();     // 新建一个list对象
+//        for (Ebook ebook : ebookList) {
+//            // 对Ebook列表进行迭代，然后拷贝到EbookResp这个List里去
+////            EbookResp ebookResp=new EbookResp();    // 1、新建一个接收对象
+////            BeanUtils.copyProperties(ebook,ebookResp);  // 2、使用Spring提供的BeanUitls.copyProperties()，参数为传递对象和接收对象
+//              // 对象赋值
+//            EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class); // source是一个对象，目标是一个类
+//
+//            respList.add(ebookResp);    // 3、将对象传递给respList
+//
+//        }
 
-        return respList;
+        // 列表复制
+        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);   // 用工具类代替上面注释掉的代码
+
+        return list;
 
 
     }
