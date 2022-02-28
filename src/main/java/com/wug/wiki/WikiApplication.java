0c69905caf3eed8e9d2ce992a8a1b47b730f1ed3
vector2myth <vector2myth@icloud.com>
2022-02-28 09:50:58 +0800

    2.3 完成电子书列表查询接口

--- src/main/java/com/wug/wiki/WikiApplication.java
+++ src/main/java/com/wug/wiki/WikiApplication.java
@@ -5,9 +5,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 //@ComponentScan({"com.wug"})
+@ComponentScan("com.wug")
 @SpringBootApplication
 @MapperScan("com.wug.wiki.mapper")
 public class WikiApplication {
---
+++ src/main/java/com/wug/wiki/domain/Category.java
@@ -0,0 +1,35 @@
+package com.wug.wiki.domain;
+
+public class Category {
+    private Long id;
+
+    private String categoryName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", categoryName=").append(categoryName);
+        sb.append("]");
+        return sb.toString();
+    }
+}
