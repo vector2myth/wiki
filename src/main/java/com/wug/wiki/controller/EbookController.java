6835581f70f58406dfef14378b78d5160680d9ee
vector2myth <vector2myth@icloud.com>
2022-02-28 20:45:00 +0800

    2.5 封装请求参数和返回参数

--- src/main/java/com/wug/wiki/controller/EbookController.java
+++ src/main/java/com/wug/wiki/controller/EbookController.java
@@ -1,7 +1,9 @@
 package com.wug.wiki.controller;
 
 import com.wug.wiki.domain.Ebook;
+import com.wug.wiki.req.EbookReq;
 import com.wug.wiki.resp.CommonResp;
+import com.wug.wiki.resp.EbookResp;
 import com.wug.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,13 +16,18 @@ import java.util.List;
 @RequestMapping("/ebook")
 public class EbookController {
 
+    // ***** 在controller层不要见到实体类，如果要用，那就新建一个副本 *****
+
     @Resource
     private EbookService ebookService;
 
+
     @GetMapping("/list")
-    public CommonResp list(){
-        CommonResp<List<Ebook>> resp = new CommonResp<>();
-        List<Ebook> list=ebookService.list();
+    public CommonResp list(EbookReq req){
+        // 传递电子书的名称，进行模糊查询
+        // 如果要传递给函数的参数很多，这样写死了只有一个参数就明显不够，就需要专门封装一个类来处理传递进来的对应实体的各种参数
+        CommonResp<List<EbookResp>> resp = new CommonResp<>();
+        List<EbookResp> list=ebookService.list(req);
 
         resp.setContent(list);
 
--- src/main/java/com/wug/wiki/service/EbookService.java
+++ src/main/java/com/wug/wiki/service/EbookService.java
@@ -1,10 +1,15 @@
 package com.wug.wiki.service;
 
 import com.wug.wiki.domain.Ebook;
+import com.wug.wiki.domain.EbookExample;
 import com.wug.wiki.mapper.EbookMapper;
+import com.wug.wiki.req.EbookReq;
+import com.wug.wiki.resp.EbookResp;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -13,7 +18,23 @@ public class EbookService {
     @Resource
     private EbookMapper ebookMapper;
 
-    public List<Ebook> list(){
-        return ebookMapper.selectByExample(null);
+    public List<EbookResp> list(EbookReq req){
+        EbookExample ebookExample=new EbookExample();
+        EbookExample.Criteria criteria = ebookExample.createCriteria();// Criteria相当于where条件
+        criteria.andNameLike("%"+req.getName()+"%"); // 模糊匹配
+        List<Ebook> ebookList=ebookMapper.selectByExample(ebookExample);
+
+        // 从ebook拷贝到ebookresp里去
+        List<EbookResp> respList=new ArrayList<>();     // 新建一个list对象
+        for (Ebook ebook : ebookList) {
+            // 对Ebook列表进行迭代，然后拷贝到EbookResp这个List里去
+            EbookResp ebookResp=new EbookResp();    // 1、新建一个接收对象
+            BeanUtils.copyProperties(ebook,ebookResp);  // 2、使用Spring提供的BeanUitls.copyProperties()，参数为传递对象和接收对象
+            respList.add(ebookResp);    // 3、将对象传递给respList
+        }
+
+        return respList;
+
+
     }
 }
