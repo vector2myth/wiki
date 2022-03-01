d844d5a857073b645883ee7b199bcca21e892e9a
vector2myth <vector2myth@icloud.com>
2022-03-01 16:35:10 +0800

    3.3 制作了The-header组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,24 +1,24 @@
 <template>
   <a-layout>
-    <a-layout-header class="header">
-      <div class="logo" />
-      <a-menu
-          theme="dark"
-          mode="horizontal"
-          v-model:selectedKeys="selectedKeys1"
-          :style="{ lineHeight: '64px' }"
-      >
-        <a-menu-item key="1">nav 1</a-menu-item>
-        <a-menu-item key="2">nav 2</a-menu-item>
-        <a-menu-item key="3">nav 3</a-menu-item>
-      </a-menu>
-    </a-layout-header>
-<router-view/>
+<!--因为有很多页面都要用到header和footer，所以可以考虑将这两个部分做成公共的组件放到component里去-->
+    <The-header/>
+      <router-view/>
     <a-layout-footer style="text-align: center">
       Ant Design ©2021 Created by Wug
     </a-layout-footer>
   </a-layout>
 </template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import TheHeader from '@/components/The-header.vue';    // 引入组件:1、引入
+
+export default defineComponent({
+  name: 'app',
+  components:{
+    TheHeader,  // 2、添加组件
+  }
+});
+</script>
 <style>
 #components-layout-demo-top-side-2 .logo {
   float: left;
---
+++ web/src/components/The-header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <a-layout-header class="header">
+      <div class="logo" />
+      <a-menu
+          theme="dark"
+          mode="horizontal"
+          v-model:selectedKeys="selectedKeys1"
+          :style="{ lineHeight: '64px' }"
+      >
+        <a-menu-item key="1">nav 123</a-menu-item>
+        <a-menu-item key="2">nav 2</a-menu-item>
+        <a-menu-item key="3">nav 3</a-menu-item>
+      </a-menu>
+    </a-layout-header>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'The-header',  // 给这个组件起的名字，就是用的时候的名字<The-header/>
+  // props: {
+  //   // 用于父子组件之间传递数据
+  //   msg: String,
+  // },
+});
+</script>
+
+<style scoped>
+
+</style>
